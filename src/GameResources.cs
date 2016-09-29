fdf14aca22905bdd2349ca689923cd9daa325a54
MingLeo <liumingvic@gmail.com>
2016-09-30 01:37:54 +1000

    Bug 7. We cannot load one of the Musics Resources.

--- src/GameResources.cs
+++ src/GameResources.cs
@@ -66,7 +66,7 @@ public static class GameResources
         NewSound("Error", "error.wav");
         NewSound("Hit", "hit.wav");
         NewSound("Sink", "sink.wav");
-        NewSound("Siren", "siren.wav");
+        //NewSound("Siren", "siren.wav");
         NewSound("Miss", "watershot.wav");
         NewSound("Winner", "winner.wav");
         NewSound("Lose", "lose.wav");
@@ -74,7 +74,7 @@ public static class GameResources
 
     private static void LoadMusic()
     {
-        NewMusic("Background", "the-strategy.mp3");
+        NewMusic("Background", "horrordrone.mp3");
     }
 
     /// <summary>
